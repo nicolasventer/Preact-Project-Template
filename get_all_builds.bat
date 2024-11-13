8ea9dc0f595e76dc4303f1f6ee932de05c7a96f8
Nicolas VENTER <thedeal78@hotmail.fr>
2024-11-13 17:52:21 +0100

    initial commit demo

---
+++ get_all_builds.bat
@@ -0,0 +1,45 @@
+rmdir /Q /S old_docs
+move docs old_docs
+mkdir docs
+
+rmdir /Q /S Preact-Project-Template
+git cl git@github.com:nicolasventer/Preact-Project-Template.git
+cd Preact-Project-Template
+
+mklink /J "Client/src/Common" "Server/src/Common"
+
+cd Server
+call bun install
+
+cd ../Client
+
+git checkout preact/full
+call bun install
+call bun run buildProd
+move dist ../../docs/preact_full
+
+git checkout react/full
+call bun install
+call bun run buildProd
+move dist ../../docs/react_full
+
+git checkout react/light
+call bun install
+call bun run buildProd
+move dist ../../docs/react_light
+
+git checkout preact/light
+call bun install
+call bun run buildProd
+move dist ../../docs/preact_light
+
+cd ../..
+
+copy old_docs\index.html docs\index.html
+
+cd script
+
+call bun install
+call bun run start
+
+cd ..
