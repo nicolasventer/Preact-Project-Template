abfa23f31ca17d7bc231a23215d8472637bc4e36
Nicolas VENTER <thedeal78@hotmail.fr>
2025-01-11 11:08:47 +0100

    react light

--- Client/src/components/_app/ExportDataButton.tsx
+++ Client/src/components/_app/ExportDataButton.tsx
@@ -1,9 +1,3 @@
 import { actions } from "@/actions/actions.impl";
-import { ActionIcon } from "@mantine/core";
-import { Save } from "lucide-react";
 
-export const ExportDataButton = () => (
-	<ActionIcon variant="transparent" onClick={actions.data.export}>
-		<Save />
-	</ActionIcon>
-);
+export const ExportDataButton = () => <button onClick={actions.data.export}>Export Data</button>;
